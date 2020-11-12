0084795abe387792d2bb0229363a7ab68995d8a1
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-11-12 17:16:14 +0300

    IndexChange.cpp

---
+++ Utils/src/IndexChange.cpp
@@ -0,0 +1,14 @@
+#include "IndexChange.h"
+
+IndexChange::IndexChange() {
+
+}
+
+int IndexChange::getIndex() {
+  return int();
+}
+
+BaseChange::TypeChange IndexChange::type() {
+  return BaseChange::TypeChange();
+}
+
