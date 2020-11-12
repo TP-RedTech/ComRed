87aa485ef468bf328dc4778f4d3dd7ecc9270b55
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-11-12 17:07:34 +0300

    added implementation of Diaposon

---
+++ Utils/src/ContentChange.cpp
@@ -0,0 +1,13 @@
+#include "ContentChange.h"
+
+ContentChange::ContentChange() {
+
+}
+
+std::string ContentChange::getContent() {
+  return std::string();
+}
+
+BaseChange::TypeChange ContentChange::type() {
+  return BaseChange::TypeChange();
+}
---
+++ Utils/src/Diaposon.cpp
@@ -0,0 +1,17 @@
+#include "Diaposon.h"
+
+Diaposon::Diaposon(size_t start, size_t end){
+
+}
+
+Diaposon::Diaposon() {
+
+}
+
+size_t Diaposon::getStartPos() {
+  return size_t();
+}
+
+size_t Diaposon::getEndPos() {
+  return size_t();
+}
