806a235b6ace83a17d98d0aa37074774a60f561b
GregoryBS <aanonimov117@gmail.com>
2020-11-12 18:51:27 +0000

    Adding some new tests for db-controller.

--- DB/inc/UserRepository.h
+++ DB/inc/UserRepository.h
@@ -7,7 +7,16 @@
 
 using namespace std;
 
-class UserRepository
+class UserRepositoryI
+{
+public:
+    UserRepositoryI(weak_ptr<AbstractDBController> ctrl);
+    virtual ~UserRepositoryI() = default;
+    virtual void createTest(User& u) = 0;
+    virtual User getTest(int id) = 0;
+};
+
+class UserRepository : public UserRepositoryI
 {
 public:
     UserRepository(weak_ptr<AbstractDBController> ctrl);
@@ -15,6 +24,15 @@ public:
 
     void createUser(User& u);
     User getById(int id);
+
+    virtual void createTest(User& u)
+    {
+        createUser(u);
+    }
+    virtual User getTest(int id)
+    {
+        return getById(id);
+    }
 
 private:
     weak_ptr<AbstractDBController> db;
--- DB/test/testDB.cpp
+++ DB/test/testDB.cpp
@@ -14,5 +14,67 @@ class MockDBController : public AbstractDBController
 {
 public:
     MOCK_METHOD2(runQuery, bool(const string&, vector<vector<string>>&));
+};
+
+class TestDBController : public ::testing::Test
+{
+protected: 
+    void SetUp()
+    {
+        db_ctrl = make_shared<MockDBController>();
+    }
+
+    void TearDown() {}
+
+    shared_ptr<MockDBController> db_ctrl;
+    vector<vector<string>> result;
+};
+
+TEST_F(TestDBController, workWithDocument)
+{
+    EXPECT_CALL(*db_ctrl, runQuery("", result)).Times(AtLeast(5));
+    DocumentRepository drep(db_ctrl);
+    Document doc;
+    User user;
+    drep.createDoc(doc);
+    drep.changeDoc(doc);
+    drep.getById(doc.getId());
+    drep.addUser(user, doc);
+    drep.getByUser(user);
 }
 
+TEST_F(TestDBController, workWithUser)
+{
+    EXPECT_CALL(*db_ctrl, runQuery("", result)).Times(AtLeast(2));
+    UserRepository urep(db_ctrl);
+    User user;
+    urep.createUser(user);
+    urep.getById(user.getId());
+}
+
+
+// TO_DO: перенести в другой файл
+/*
+class MockUserRepository : public UserRepositoryI
+{
+public:
+    explicit MockUserRepository(weak_ptr<AbstractDBController> db) : 
+        UserRepositoryI(db) {}
+
+    MOCK_METHOD1(createTest, void(User&));
+    MOCK_METHOD1(getTest, User(int));
+};
+
+TEST(UserRepository, methodsCall)
+{
+    shared_ptr<DBController> db_ctrl = make_shared<DBController>();
+    MockUserRepository murep(db_ctrl);
+    UserManager manager(murep);
+    User user;
+    EXPECT_CALL(murep, createTest(user)).Times(AtLeast(1));
+    EXPECT_CALL(murep, getTest(user.getId())).Times(AtLeast(1));
+    manager.create("");
+    managet.getById(user.getId());
+}
+
+*/
