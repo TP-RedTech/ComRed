03052a5d5e16c35e454e972c878af7e48823d097
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-11-12 14:58:57 +0300

    added declaration of Diaposon

---
+++ Utils/include/Diaposon.h
@@ -0,0 +1,19 @@
+#ifndef UTILS_INCLUDE_DIAPOSON_H_
+#define UTILS_INCLUDE_DIAPOSON_H_
+
+#include <iostream>
+
+class Diaposon {
+ private:
+  size_t startPos;
+  size_t endPos;
+
+ public:
+  Diaposon(size_t start, size_t end);
+
+  size_t getStartPos();
+
+  size_t getEndPos();
+};
+
+#endif //UTILS_INCLUDE_DIAPOSON_H_
