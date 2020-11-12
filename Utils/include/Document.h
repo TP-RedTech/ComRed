8ed00f67a262af40998231b778beecc6442409bb
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-11-12 14:15:30 +0300

    declaration of Document added

---
+++ Utils/include/Document.h
@@ -0,0 +1,44 @@
+#ifndef UTILS_INCLUDE_DOCUMENT_H_
+#define UTILS_INCLUDE_DOCUMENT_H_
+
+#include <iostream>
+#include <vector>
+
+#include "BaseChange.h"
+
+class Operation{
+ private:
+  int revision;
+  int idEditor;
+  size_t lengthBeforeOperation;
+  size_t lengthAfterOperation;
+  std::vector<BaseChange> changes;
+
+ public:
+  Operation();
+
+  void setRevision(int newRevision);
+
+  int getRevision() const;
+
+  void setIdEditor(int newId);
+
+  int getIdEditor() const;
+
+  void setLengthBeforeOperation(size_t newLength);
+
+  void setLengthAfterOperation(size_t newLength);
+
+  size_t getLengthBeforeOperation() const;
+
+  size_t getLengthAfterOperation() const;
+
+  size_t getSizeOfChanges() const;
+
+  void addChange(BaseChange newChange);
+
+  Operation mergeOperations(Operation anotherOperation);
+
+};
+
+#endif //UTILS_INCLUDE_DOCUMENT_H_
