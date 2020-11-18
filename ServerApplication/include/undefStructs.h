15d94a04ab6195220fd7891323fa2d9f6e72562a
Timonin Anton <dotruger.37@gmail.com>
2020-11-18 21:38:15 +0300

    Add updated uml files

---
+++ ServerApplication/include/undefStructs.h
@@ -0,0 +1,18 @@
+#ifndef UNTITLED_UNDEFSTRUCTS_H
+#define UNTITLED_UNDEFSTRUCTS_H
+
+struct Operation{};
+
+struct Document{
+    Document() = default;
+};
+
+struct DocumentRepository {
+
+};
+
+struct UserRepository {
+
+};
+
+#endif //UNTITLED_UNDEFSTRUCTS_H
