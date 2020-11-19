c7b8961dc6154f2dbd4f529492eb30e163ddaf9e
Paul <tscheklin@gmail.coj>
2020-11-19 16:09:27 +0300

    updated tests

--- HTTPServer/test/controllers_test.cpp
+++ HTTPServer/test/controllers_test.cpp
@@ -17,41 +17,85 @@
 using namespace http;
 
 class MockClient : public Client {
+public:
   MockClient() = default;
   MOCK_METHOD0(run, void());
 };
 
 class MockConnectController : public server::ConnectController {
+public:
   MockConnectController() = default;
   MOCK_METHOD1(handleRequest, Reply(const Request &request));
 };
 
 class MockCreateController : public server::CreateController {
+public:
   MockCreateController() = default;
   MOCK_METHOD1(handleRequest, Reply(const Request &request));
 };
 
 class MockDeleteController : public server::DeleteController {
+public:
   MockDeleteController() = default;
   MOCK_METHOD1(handleRequest, Reply(const Request &request));
 };
 
 class MockEditController : public server::EditController {
+public:
   MockEditController() = default;
   MOCK_METHOD1(handleRequest, Reply(const Request &request));
 };
 
 class MockLoginController : public server::LoginController {
+public:
   MockLoginController() = default;
   MOCK_METHOD1(handleRequest, Reply(const Request &request));
 };
 
 class MockLogoutController : public server::LogoutController {
+public:
   MockLogoutController() = default;
   MOCK_METHOD1(handleRequest, Reply(const Request &request));
 };
 
+class MockRegistrationController : public server::RegistrationController {
+public:
+  MockRegistrationController() = default;
+  MOCK_METHOD1(handleRequest, Reply(const Request &request));
+};
 
-TEST(test, test) {
-  ASSERT_EQ(true, true);
+class ConnectContorllerTest : public ::testing::Test {
+protected:
+  void SetUp() override
+  {
+
+  }
+
+};
+
+TEST(ConnectControllerTest, test1) {
+}
+
+TEST(CreateControllerTest, test1) {
+  MockCreateController c;
+}
+
+TEST(DeleteControllerTest, test1) {
+  MockDeleteController c;
+}
+
+TEST(EditControllerTest, test1) {
+  MockEditController c;
+}
+
+TEST(LoginControllerTest, test1) {
+  MockLoginController c;
+}
+
+TEST(LogoutControllerTest, test1) {
+  MockLogoutController c;
+}
+
+TEST(RegistrationControllerTest, test1) {
+  MockRegistrationController c;
 }
