3bcdee7103ec2643baa73ec49a252baa2ad429d9
GregoryBS <aanonimov117@gmail.com>
2020-12-09 07:32:15 +0000

    Adding first implement of db-controller.

--- DB/src/DBController.cpp
+++ DB/src/DBController.cpp
@@ -1,14 +1,80 @@
 #include "../include/DBController.h"
 
-DBController::DBController(int size) {}
+DBController::DBController(int size) 
+{
+    createPool(size);
+}
 
-DBController::~DBController() {}
+DBController::~DBController() 
+{
+
+}
 
 bool DBController::runQuery(const string& query, 
-                            vector<vector<string>>& result) {}
+                            vector<vector<string>>& result) 
+{
+    auto connect = getConnection();
+    pqxx::work worker(*connect.get());
+    bool flag = true;
+    try
+    {
+        pqxx::result r = worker.exec(query);
+        worker.commit();
+        result.clear();
+        for (auto row: r)
+        {
+            result.push_back(vector<string>());
+            for (int i = 0; i < row.size(); i++)
+                result.back().push_back(row[i].as<string>());
+        }
+    }
+    catch(const std::exception& e)
+    {
+        cerr << e.what() << endl;
+        cerr << "Query: " << query << endl;
+        flag = false;
+    }
+    freeConnection(connect);
+    return flag;
+}
 
-void DBController::createPool(int size) {}
+void DBController::createPool(int size) 
+{
+    string connectString = "";
+    ifstream fin(CONFIG, ios_base::in);
+    if (fin.is_open()) 
+    {
+        fin >> connectString;
+        fin.close();
+    }
+    try
+    {
+        for (int i = 0; i < size; ++i) 
+            connectionPool.push(make_shared<pqxx::connection>(connectString));
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << endl;
+    }
+}
 
-pqxx::connection& DBController::getConnection(void) {}
+shared_ptr<pqxx::connection> DBController::getConnection(void) 
+{
+    unique_lock<mutex> lock(mtx);
+    while (connectionPool.empty()) 
+    {
+        cond.wait(lock);
+    }
 
-void DBController::freeConnection(pqxx::connection& connection) {}
+    auto connect = connectionPool.front();
+    connectionPool.pop();
+    return connect;
+}
+
+void DBController::freeConnection(shared_ptr<pqxx::connection> connection) 
+{
+    unique_lock<mutex> lock(mtx);
+    connectionPool.push(connection);
+    lock.unlock();
+    cond.notify_one();
+}
